7a3e21b8f31ec1dbc2ebcc711898321033070a6e
Siroshun09 <siro.shun8@gmail.com>
2020-08-29 17:23:13 +0900

    [Clean] 不要なメソッドを削除する

--- src/main/java/net/okocraft/punishmentnotifier/PunishmentNotifier.java
+++ src/main/java/net/okocraft/punishmentnotifier/PunishmentNotifier.java
@@ -1,7 +1,5 @@
 package net.okocraft.punishmentnotifier;
 
-import club.minnced.discord.webhook.WebhookClient;
-import club.minnced.discord.webhook.WebhookClientBuilder;
 import com.github.siroshun09.configapi.bungee.BungeeConfig;
 import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.chat.TextComponent;
@@ -11,7 +9,6 @@ import net.md_5.bungee.api.plugin.Plugin;
 public class PunishmentNotifier extends Plugin {
 
     private BungeeConfig config;
-    private WebhookClient webhook;
     private DiscordNotifier discordNotifier;
     private PlayerNotifier playerNotifier;
 
@@ -44,11 +41,8 @@ public class PunishmentNotifier extends Plugin {
 
         unregisterListeners();
         getProxy().getPluginManager().unregisterCommands(this);
-        getLogger().info("Successfully disabled!");
-    }
 
-    public WebhookClient getWebhook() {
-        return webhook;
+        getLogger().info("Successfully disabled!");
     }
 
     public DiscordNotifier getDiscordNotifier() {
@@ -57,17 +51,6 @@ public class PunishmentNotifier extends Plugin {
 
     public PlayerNotifier getPlayerNotifier() {
         return playerNotifier;
-    }
-
-    private void createWebhook(String url) {
-        if (url.isEmpty()) {
-            getLogger().warning("No Webhook url has been set.");
-            return;
-        }
-
-        webhook = new WebhookClientBuilder(url)
-                .setThreadFactory(r -> new Thread(r, "Punishment-Notification-Thread"))
-                .setWait(true).build();
     }
 
     private void registerListeners() {
